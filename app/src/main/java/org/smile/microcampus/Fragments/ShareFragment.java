013023054f5785c776ca7b9a1f3b9a7b95e68767
ASCPJ <18027823708@163.com>
2015-12-11 20:56:02 +0800

    解决冲突

--- app/src/main/java/org/smile/microcampus/Fragments/ShareFragment.java
+++ app/src/main/java/org/smile/microcampus/Fragments/ShareFragment.java
@@ -13,6 +13,7 @@ import org.smile.microcampus.R;
 
 import io.rong.imkit.RongIM;
 import io.rong.imlib.RongIMClient;
+import io.rong.imlib.model.Conversation;
 
 /**
  * 分享模块
@@ -27,14 +28,37 @@ public class ShareFragment extends Fragment implements View.OnClickListener{
         view  = inflater.inflate(R.layout.fragment_share, container, false);
         Button button=(Button)view.findViewById(R.id.star_chat);
         button.setOnClickListener(this);
-        
 
-        return view;
+        String Token = "2WpK2Lbs4r3DG8/X7JgVsehfvVfP+0RfM/vke2v2ZOfLtzGIgs5WOVycN8torIffl8I1zwwew5RdA8UVa9A7ag==";//test
+        /**
+         * IMKit SDK调用第二步
+         *
+         * 建立与服务器的连接
+         *
+         */
+        RongIM.connect(Token, new RongIMClient.ConnectCallback() {
+            @Override
+            public void onTokenIncorrect() {
+                //Connect Token 失效的状态处理，需要重新获取 Token
+            }
+
+            @Override
+            public void onSuccess(String userId) {
+                Log.e("MainActivity", "——onSuccess— -" + userId);
+            }
+
+            @Override
+            public void onError(RongIMClient.ErrorCode errorCode) {
+                Log.e("MainActivity", "——onError— -" + errorCode);
+            }
+        });
+
+    return view;
     }
 
 
     @Override
     public void onClick(View v) {
-        RongIM.getInstance().startPrivateChat(getActivity(), "1", "hello");
+        RongIM.getInstance().startConversation(getActivity(),Conversation.ConversationType.CHATROOM, "chatroom001", "融云客服");
     }
 }
